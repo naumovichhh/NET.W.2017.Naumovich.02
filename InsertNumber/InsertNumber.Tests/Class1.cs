269899b2a3a06ff7a60c3e9d10ca24628437eeaf
naumovichhh <32872842+naumovichhh@users.noreply.github.com>
2017-10-23 16:01:50 +0300

    Add files via upload

---
+++ InsertNumber/InsertNumber.Tests/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Day2;
+
+namespace Day2.Tests
+{
+    public static class InsertNumberTests
+    {
+        [TestCase(15, 15, 0, 0, 15)]
+        [TestCase(8, 15, 0, 0, 9)]
+        [TestCase(8, 15, 3, 8, 120)]
+        public static void InsertTest(int n1, int n2, int i, int j, int result)
+        {
+            Assert.AreEqual(result, InsertNumber.Insert(n1, n2, i, j));
+        }
+    }
+}
---
+++ InsertNumber/InsertNumber.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("InsertNumber.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("InsertNumber.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("47db1ff8-1fe6-49ac-bfb9-078638fa128b")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
